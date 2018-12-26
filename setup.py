d7f11ab2978f79ff5694643da42825de6f866926
Joe A Christoff <JCHRISTO@amfam.com>
2018-12-26 15:16:25 -0600

    Add author information

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
+
 setup(name='python_udf',
       version='0.1',
       packages=find_packages(),
+      author='Joe Christoff',
+      author_email='christoffjoe@gmail.com',
       )
