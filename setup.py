4d39f758648bb030f2e66ae5462a53329f4d2a1b
Joe A Christoff <JCHRISTO@amfam.com>
2018-12-26 15:17:07 -0600

    Add description info

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(name='python_udf',
       version='0.1',
       packages=find_packages(),
+      description='User defined functions for personal use',
       author='Joe Christoff',
       author_email='christoffjoe@gmail.com',
       )
