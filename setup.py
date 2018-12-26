cbe9bc9daec651df879359349e6560ddef8c979c
Joe Christoff <40745710+jchristo12@users.noreply.github.com>
2018-12-26 15:40:27 -0600

    Update setup.py
    Update packages

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(name='python_udf',
       version='0.1',
-      packages=find_packages(),
+      packages=['python_pkg'],
       description='User defined functions for personal use',
       author='Joe Christoff',
       author_email='christoffjoe@gmail.com',
