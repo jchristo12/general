5765c1def4d499dca35c3ddc65c0e5ee857b7abd
Joe <christoffjoe@gmail.com>
2019-06-01 22:28:36 -0500

    update python_udf version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='python_udf',
-      version='0.3.2',
+      version='0.3.3',
       packages=['python_pkg'],
       description='User defined functions for personal use',
       author='Joe Christoff',
